903c6382b6ca88544b6ff06a939034aaf3b1c76b
NgTienToan <43052549+NgTienToan@users.noreply.github.com>
2020-02-29 16:15:32 +0700

    get api
    get api

--- food-recipe/src/App.js
+++ food-recipe/src/App.js
@@ -1,27 +1,49 @@
-import React, {useState} from 'react';
-import './App.css';
-import Header from './components/Header'
-import Recipe from './components/Recipe'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Recipes from "./components/Recipe";
+import Axios from "axios";
 
 function App() {
-
-  const [search, setSearch] = useState("");
+  const [search, setSerach] = useState("chiken");
   const [recipes, setRecipes] = useState([]);
 
   const APP_ID = "a9e97685";
   const APP_KEY = "9709c0401aa79a15eb622257a4358c0b";
 
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    setRecipes(res.data.hits);
+  };
+
   const onInputChange = e => {
-    console.log(e.target.value);
-    setSearch(e.target.value)
-    
-  }
+    setSerach(e.target.value);
+  };
+
+  const onSearchClick = () => {
+    getRecipes();
+  };
   return (
     <div className="App">
-      <Header search={search} onInputChange={onInputChange} />
-      <Recipe />
+      <Header
+        search={search}
+        onInputChange={onInputChange}
+        onSearchClick={onSearchClick}
+      />
+      <div className="container">
+        <Recipes recipes={recipes} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
